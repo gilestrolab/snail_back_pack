cf757b680c903bfbeeafa1fe9b261e33886a3b78
Quentin Geissmann <qgeissmann@gmail.com>
2014-11-10 15:43:48 +0000

    fix sparse oversampling

--- arduino_prototypes/sparse_phototransistor/sparse_phototransistor.ino
+++ arduino_prototypes/sparse_phototransistor/sparse_phototransistor.ino
@@ -8,10 +8,10 @@
    delay() statement. */
  
 #define IN_PIN 0
-#define OVER_SAMPLING 5.
+#define OVER_SAMPLING 3.
 #define FS 5.
 #define POWER_PIN 13
-#define RISE_TIME_PHOTOTRANSISTOR 300 // us
+#define RISE_TIME_PHOTOTRANSISTOR 1// ms
  
 void setup(void) {
 	Serial.begin(9600); 
@@ -20,16 +20,18 @@ void setup(void) {
 
 int read(void){
 	int accum = 0; 
-	for(int i =0; i !=OVER_SAMPLING; ++i){
-		digitalWrite(POWER_PIN, HIGH);   
-		delayMicroseconds(RISE_TIME_PHOTOTRANSISTOR);
+	for(int i =0; i < OVER_SAMPLING; i++){
+		digitalWrite(POWER_PIN, HIGH);
+		delay(RISE_TIME_PHOTOTRANSISTOR);
 		int pinRead0 = analogRead(IN_PIN);
 		digitalWrite(POWER_PIN, LOW);   
 		accum += pinRead0;
-		delayMicroseconds(1e6/(FS*OVER_SAMPLING) - RISE_TIME_PHOTOTRANSISTOR);
+		
+		float to_sleep_us = 1e6/(FS*OVER_SAMPLING) ;
+		delay(1e-3 * to_sleep_us - RISE_TIME_PHOTOTRANSISTOR) ;
 	}
-	int out = (int) accum / OVER_SAMPLING;
-	return out;
+	//int out =  accum / OVER_SAMPLING;
+	return accum;
 }
 void loop(void) {
   

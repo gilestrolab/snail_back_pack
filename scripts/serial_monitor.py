b1be1d92c205fb2aad3f8f758866fa692dda0b61
Quentin Geissmann <qgeissmann@gmail.com>
2015-01-25 18:30:02 +0000

    simplify and comment the serial visualisation script

--- scripts/serial_monitor.py
+++ scripts/serial_monitor.py
@@ -3,132 +3,117 @@ import os
 import time
 import argparse
 import numpy as np
-from scipy.interpolate import interp1d
 from collections import deque
 import pygame
 
 
 _BAUD = 57600
 _N_POINTS = 200
-colour_map = [
-            (255,0,0),
-            (0,255,0),
-            (0,0,255),
-            (0,255,255),
-            (255,255,0),
-            (255,0,255)
-            ]
+DISPLAY_WIDTH, DISPLAY_HEIGHT = 1200, 520
 
+
+def plot(screen, time_queue, value_queue, scale=True):
+    
+    t = np.array(list(time_queue))
+    values = np.array(list(value_queue), dtype=np.float32)
+    
+    
+    # we scale the signal so that all vallue lie between 0 and 1 
+    new_y =  values - np.min(values,0)
+    
+    mmax = np.max(new_y,0)
+    
+    # we ensure no division by 0 will happen
+    if mmax == 0:
+        return
+        
+    new_y /= np.max(new_y,0)
+    
+    
+    new_y *= DISPLAY_HEIGHT
+    new_y = DISPLAY_HEIGHT - new_y
+    
+    #we express time as a proportion of the window size
+    
+    new_t = t - t[0]
+    new_t *= (DISPLAY_WIDTH/float(window_size))
+
+    screen.fill((0,0,0))
+    pts = [(int(x),int(y)) for x,y in zip(new_t, new_y )]
+    pygame.draw.lines(screen, (255, 255, 0),False, pts, 3)
+    pygame.display.flip()
+    
+     
 if __name__ == "__main__":
-    import argparse
+    
+    
+    # parsing command line arguments
     parser = argparse.ArgumentParser()
+    # TODO windows compat default port !
     parser.add_argument('--port', help='port', type=str, default="/dev/ttyACM0")
-    parser.add_argument('--vws', help='viewing window size (s)', type=int, default=20)
-    parser.add_argument('--out', help='an optional output file', type=str, default=os.devnull)
-    # parser.add_argument('--print', help='stdout?', type=bool, default=False)
+    parser.add_argument('--vws', help='The duration of the viewing window (s)', type=int, default=20)
+    parser.add_argument('--out', help='An optional output file', type=str, default=os.devnull)
 
-    scale = True
     args = parser.parse_args()
-
     arg_dict = vars(args)
-
-
     window_size = arg_dict["vws"]
-
+    out_file = arg_dict["out"]
+    
+    # Here we open the serial port
     serial_port = serial.Serial(arg_dict["port"], _BAUD, timeout=2)
     
+    # We start a timer using the real time from the operating system
     start = time.time()
-    time_queue = deque()
-    value_queue = deque()
-
-
-    pygame.init()
-
-    width, height = 1500, 900
-    screen = pygame.display.set_mode((width, height))
     
+    # Then we make two queues (FIFO containers); one for the values and one for the time stamps.
+    time_queue, value_queue = deque(), deque()
 
-    with open(arg_dict["out"], "w") as f:
+    # We need to initialise pygame
+    pygame.init()
+    # We build the pygame window before we can start painting inside 
+    
+    display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    
+    # The `with statement` will ensure the file is closed if an exception happens
+    
+    with open(out_file, "w") as f:
+        # infinite loop
         while True:
+            # we read a line from serial port and remove any `\r` and `\n` character
             line = serial_port.readline().rstrip()
-            
-            if line == "":
-                continue
+            # just after, we get a time stamp
+            now = time.time()
+            # we try to convert the line to an integer value
             try:
-                values =  [int(c) for c in line.split(",") if c]
-                #~ int_value = int(line.rstrip())
-                values = np.array(values, dtype=np.float32)
-                
+                value =  int(line)
+            # if something goes wrong, we do not stop, but we print the error message
             except ValueError as e:
                 print e
                 continue
             
-            now = time.time()
+            # the relative time from the start of the program is `dt`
             dt =  now - start
-#~ 
-            time_queue.append(dt)
-            value_queue.append(values)
             
-            if time_queue[-1] < 3 or len(time_queue) < 10:
+            # We append relative  time and value to their respective queues
+            time_queue.append(dt)
+            value_queue.append(value)
+            
+            # we wait to have at least five points AND three seconds of data
+            if time_queue[-1] < 3 or len(time_queue) < 5:
                 continue
 
+            # Now, we remove/forget from the queues any value older than the window size
+            # this way we will only plot the last n (default 20) seconds of data
             while time_queue[-1] - time_queue[0] > window_size:
                 time_queue.popleft()
                 value_queue.popleft()
                 
+            # Now we plot the values
+            plot(display, time_queue, value_queue)
             
-            
-            t = np.array(list(time_queue))
-            y = np.array(list(value_queue))
-            
-            if y.dtype == object:
-                time_queue = deque()
-                value_queue = deque()
-                continue
-            
-            new_t = t
-            new_y = np.copy(y)
-
-            if scale:
-                
-                
-                new_y -= np.min(new_y,0)
-                mmax = np.max(new_y,0)
-                if np.any(mmax == 0):
-                    continue
-                new_y /= np.max(new_y,0)
-            
-            
-            new_y *= height
-            
-            new_y = height - new_y
-
-            new_t -= new_t[0]
-            new_t *= (width/float(window_size))
-            
-            
-            screen.fill((0,0,0))
-            
-            n_series = y.shape[1]
-            for k in range(n_series):
-                y_coords = new_y[:,k]
-
-                y_coords /= float(n_series)
-                y_coords += (k * height / float(n_series))
-                
-                pts = [(int(x),int(y)) for x,y in zip(new_t, y_coords )]
-            
-                pygame.draw.lines(screen, colour_map[k],False, pts, 3)
-                hline =[
-                        (0,(k * height / float(n_series))),
-                        (width,(k * height / float(n_series)))]
-                
-                
-                pygame.draw.lines(screen, colour_map[k],False, pts, 3)
-                pygame.draw.aalines(screen, (255,255,255),False, hline, 3)
-            
-            pygame.display.flip()
-            
-            print "%f, %s\n" % (now,line)
+            # So that the program stops if we close the window
+            for et in pygame.event.get():
+                if et.type == pygame.QUIT:
+                    raise KeyboardInterrupt
             
 

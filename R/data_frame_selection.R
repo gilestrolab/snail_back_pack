ae88d20f86d377f5efce8c353fa66dc62a07c1bb
alysiaymk <alysia.kwan14@imperial.ac.uk>
2015-04-23 15:27:00 +0100

    deleted old code

--- R/data_frame_selection.R
+++ R/data_frame_selection.R
@@ -7,7 +7,7 @@ SAMPLING_FREQUENCY <- 5 # in Hz
 my_freq_fun <- function(y, fs){
 	sp <- seewave::spec(y,f=fs)
 	f <- seewave::fpeaks(sp, f=fs,nmax=1)
-	return(f[1,1])
+	return(f[1,1]*1000)
 }
 
 my_freq_fun_runmed <- function(y, fs,k){
@@ -17,7 +17,7 @@ my_freq_fun_runmed <- function(y, fs,k){
 	sp <- seewave::spec(ny,f=fs)
 	f <- seewave::fpeaks(sp, f=fs,nmax=1)
 	
-	return(f[1,1])
+	return(f[1,1]*1000)
 }
 
 
@@ -34,12 +34,9 @@ heterogeneous_df <- read.csv(DATA_FILE, head=F)
 colnames(heterogeneous_df) <- c("t", "y")
 
 
-# interpolation goes here
-# it generates a ned df called `df`
+# interpolation, generates df
 out_t <- seq(from=0, to=max(heterogeneous_df$t), by= 1/SAMPLING_FREQUENCY)
 df_v1 <- approx(x=heterogeneous_df$t, y=heterogeneous_df$y, xout=out_t, method='linear')
-
-
 df_v2 <- data.frame(df_v1)
 colnames(df_v2) <- c("t", "y")
 df <- na.omit(df_v2)
@@ -49,6 +46,7 @@ df$min <- floor(df$t /60) + 1
 mins_of_interest_df <- subset(df, df$min %in% ref_df$min)
 list_of_mins <- split(mins_of_interest_df, mins_of_interest_df$min)
 
+
 pdf("plot.pdf",w=16,h=9)
 lapply(list_of_mins, function(sdf){
 		title <- sdf$min[1]
@@ -57,44 +55,12 @@ lapply(list_of_mins, function(sdf){
 dev.off()
 
 
-freq_meth1 <- sapply(list_of_mins, function(sdf){
-		sp <- seewave::spec(sdf$y,f=SAMPLING_FREQUENCY)
-		f <- seewave::fpeaks(sp, f=SAMPLING_FREQUENCY,nmax=1)
-	})
-
-
-freqdf1 <- data.frame(freq_meth1)
-freq1 <- sapply(freqdf1, "[", 1)
-ref_df <- cbind(ref_df,freq1)
-
-
-freq_meth2 <- sapply(list_of_mins, k=51, function(sdf, k){
-		low_c <- runmed(sdf$y, k)
-		sp <- seewave::spec(low_c,f=SAMPLING_FREQUENCY)
-		f <- seewave::fpeaks(sp, f=SAMPLING_FREQUENCY,nmax=1)
-		f*1000
-	})
-
-	
-freqdf2 <- data.frame(freq_meth2)
-freq2 <- sapply(freqdf2, "[", 1)
-ref_df <- cbind(ref_df,freq2)
-
-
-plot(freq1 ~ of, ref_df, pch=as.character(q), col=q)
-mod <- lm(freq1 ~ of, ref_df)
-
-test <- list_of_mins[[1]]
-plot(y ~ t,test, type='l')
-test$low_comp <- runmed(test$y,k=51)
-lines(low_comp ~ t,test, col="red")
-test$high_comp <- test$y - test$low_comp
-plot(high_comp ~ t,test, type='l')
-
 
 results <- list(
-	meth_runmed51 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=51),
-	meth_runmed25 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=25),
+	#meth_runmed51 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=51),
+	meth_runmed12 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=12),
+	#meth_runmed37 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=37),
+	#meth_runmed43 = apply_freq_meth(chunks=list_of_mins, my_freq_fun_runmed, fs=5, k=43),
 	meth_first = apply_freq_meth(chunks=list_of_mins, my_freq_fun, fs=5)
 	)
 
@@ -114,3 +80,20 @@ long_df <- merge(ref_df, tmp_df)
 	
 ggplot(long_df,aes(y = fc, x =of,colour=method,shape=method)) +
 geom_point() + geom_smooth(method="lm", fill=NA)
+
+
+
+
+#################
+
+plot(freq1 ~ of, ref_df, pch=as.character(q), col=q)
+mod <- lm(freq1 ~ of, ref_df)
+
+test <- list_of_mins[[1]]
+plot(y ~ t,test, type='l')
+test$low_comp <- runmed(test$y,k=51)
+lines(low_comp ~ t,test, col="red")
+test$high_comp <- test$y - test$low_comp
+plot(high_comp ~ t,test, type='l')
+
+##################

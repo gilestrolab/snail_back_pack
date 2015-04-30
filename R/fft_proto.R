5a76294aedbfa197052e10265015d6ebeb044b4c
Quentin Geissmann <qgeissmann@gmail.com>
2015-04-30 19:36:05 +0100

    add spectrum prototype

---
+++ R/fft_proto.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+
+set.seed(1)
+beats <- sin(seq(from=0,to=40*2*pi,length.out=300))
+
+low_fs_noise <- sin(seq(from=0,to=3*2*pi,length.out=300))  * 3
+
+white_noise <- rnorm(300, 0, .2)
+
+fake_ts <- low_fs_noise + beats + white_noise
+
+plot(fake_ts, t='l')
+
+#oce::pwelch 
+plot(pspectrum(fake_ts))

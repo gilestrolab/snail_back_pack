da61434ea47c56086310b8bc20c54b48838b2980
ruthmoon <ruth.moon11@imperial.ac.uk>
2015-05-05 19:56:37 +0100

    Create snail_temperature_sensor.ino

---
+++ experiments/snail_temperature_experiment/snail_temperature_sensor.ino
@@ -0,0 +1,42 @@
+int time_on = 25;//ms
+int sampling_frequency_sec = 30;
+int time_off = sampling_frequency_sec *1000; // sampling periode ms
+int temperature_sensor_pin = 5;
+int N = 64; //oversampling
+float over_sampling_delay(sampling_frequency_sec/N);
+
+#define aref_voltage 3.3;  // we tie 3.3V to ARef and measure it with a multimeter!
+float temperature_raw;
+
+void setup() {
+	// put your setup code here, to run once:
+	Serial.begin(57600);
+        pinMode(temperature_sensor_pin,INPUT);
+	analogReference(EXTERNAL); //to set aref at 3.3V
+}
+
+void loop() {
+        
+        unsigned long t0 = millis();
+        float accum = 0;
+        
+        for(int i = 0; i < N; i++) {
+	   accum += analogRead(temperature_sensor_pin); // increment the accumulator by the value in the phototransistor pin
+	   delay(over_sampling_delay);
+           }
+
+        accum = accum / N;
+        
+        float voltage = accum * aref_voltage; //float voltage = temperature_raw;//convert raw temperature reading to degrees celcius
+        voltage /= 1024.0; 
+        float temperature_C = (voltage - 0.5) * 100 ; //converting from 10 mv per degree wit 500 mV offset
+        Serial.println(temperature_C); // now print out the temperature //Serial.println(" degrees C");
+        
+        unsigned long t1 = millis();
+        unsigned long dt = t1 - t0;
+	
+        delay(time_off - dt);
+          
+
+}
+

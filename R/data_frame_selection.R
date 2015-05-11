87977bccd0a5a1d02affafef6167c9f2cc103e50
alysiaymk <alysia.kwan14@imperial.ac.uk>
2015-05-11 16:13:04 +0100

    w/ interpolation, w/o acf

--- R/data_frame_selection.R
+++ R/data_frame_selection.R
@@ -3,7 +3,7 @@ library(seewave)
 library(psd)
 library(mFilter)
 source("~/Documents/snail_back_pack/R/funs.R")
-DATA_FILE <- "/home/alysia/Documents/snail_back_pack/R/heart_009.csv"
+DATA_FILE <- "/home/alysia/Documents/snail_back_pack/R/ref_data_heart_snail.txt"
 REF_FILE <- "/home/alysia/Documents/ref.csv"
 SAMPLING_FREQUENCY <- 5 # in Hz
 

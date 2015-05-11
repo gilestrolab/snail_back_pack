e75f1b78643dacc9790bf45c9da0927afd8aa123
alysiaymk <alysia.kwan14@imperial.ac.uk>
2015-05-11 17:37:59 +0100

    changed pdf generation file

---
+++ R/pdf_generation(changed_variables).R
@@ -0,0 +1,25 @@
+
+
+DATA_FILE <- "heart_011.csv"
+SAMPLING_FREQUENCY <- 5 # in Hz
+
+heterogeneous_df <- read.csv(DATA_FILE, head=F)
+colnames(heterogeneous_df) <- c("t", "y")
+df_v1 <- approx(x=heterogeneous_df$t, y=heterogeneous_df$y, xout=heterogeneous_df$t, method='linear')
+df_v2 <- data.frame(df_v1)
+colnames(df_v2) <- c("t", "y")
+df <- na.omit(df_v2)
+
+
+df$min <- round(df$t /60)
+ldf <- split(df, df$min)
+pdf("visualtest.pdf",w=16,h=9)
+par(mfrow=c(2,1))
+for(d in ldf){
+h <- round(d$t[1] / 3600,3)
+title <- paste0("Starts at h = ", h)
+print(title)
+plot(y ~ t, d, type='l', main=title)
+
+}
+dev.off()

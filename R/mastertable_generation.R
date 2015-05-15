08b83865ef757c6c9fe19b8edf4eaf1292762856
alysiaymk <alysia.kwan14@imperial.ac.uk>
2015-05-15 16:02:15 +0100

    .

--- R/mastertable_generation.R
+++ R/mastertable_generation.R
@@ -5,49 +5,52 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-master_table_v1 <- data.table(read.csv("~/Documents/snail_back_pack/experiments/snail_temperature_experiment/results/master_table.csv", header=T, na.strings=" "))
-master_table_v2 <- na.omit(master_table_v1[1:16])
-
-
-
-
-
 pre_temp_1112 <- fread("~/Documents/temp_011_012.csv", sep=",", header=F, data.table=F)
 
 li_11 <- fread("~/Documents/heart_011.csv", sep=",", header=F, data.table=F)
-li_12 <- fread("~/Documents/heart_012.csv", sep=",", header=F, data.table=F)
 
 
 
 
-s5_list <- list(li_11, li_12)
-s5_bound <- rbindlist(s5_list, use.names=T, fill=T)
-
-df$min <- floor(df$t /60) + 1
-mins_of_interest_df <- subset(df, df$min %in% ref_df$min)
-list_of_mins <- split(mins_of_interest_df, mins_of_interest_df$min)
 
 
 
-apply_freq_meth(chunk=heterogeneous_df, interp_fun, fs=5)
+
+
+
+
+
+
+
+
+master_table_v1 <- data.table(read.csv("~/Documents/snail_back_pack/experiments/snail_temperature_experiment/results/master_table.csv", header=T, na.strings=" ", skip=1))
+master_table_v2 <- na.omit(master_table_v1[1:16])
 
 
 heart_table <- sapply(master_table_v2$heart_file, fread("~/Documents/", sep=",", header=F, data.table=F))
+heart_table <- sapply(master_table_v2$heart_file, function(s){
+	fread("~/Documents/"s, sep=",", header=F, data.table=F)
+})
+
+DF1 <- NULL
+for(file in filecol){
+	
+	newfile <- gsub("\\s", "", file)
+	files <- read.csv(newfile, sep=",", header=F)
+	DF1 <- rbind(DF1, files)
+}
+
+filecol <- data.table(gsub("\\s", "", master_table_v2$heart_file))
 
 
+heart_table <- sapply(filecol, read.csv)
 
-#Error in fread("~/Documents/", sep = ",", header = F, data.table = F) : 
-#Opened file ok, obtained its size on disk (0.0MB), but couldn't memory map it. This is a 64bit machine so this is surprising. Please report to datatable-help.
+
+files <- list.files(pattern="heart")
+
+DF2 <- NULL
+for (f in files) {
+   heart_data <- read.csv(f, header=F, sep=",")
+   DF2 <- rbind(DF2, data.frame(heart_data))
+}
+
